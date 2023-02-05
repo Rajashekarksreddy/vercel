27fe4146884bf23699787ed8be6d564c78bd5036
RajashekarReddy <rajksreddy@gmail.com>
2023-02-05 22:58:56 +0530

    word stats

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
-import '@/styles/globals.css'
+import { Lora } from '@next/font/google'
+import Head from 'next/head'
+
+import '../public/output.css'
+
+const lora = Lora({ weight: ['400', '500'], subsets: ['latin'] })
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <main className={lora.className}>
+            <Head>
+                <title>wordstats</title>
+            </Head>
+
+            <Component {...pageProps} />
+        </main>
+    )
 }
